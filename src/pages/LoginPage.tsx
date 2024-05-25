436c888968040dc7b451087ce79a31116004669c
dd <4daveid@gmail.com>
2024-05-25 18:43:39 -0400

    check test

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,13 +5,13 @@ import { OrbitControls } from "@react-three/drei";
 
 export const LoginPage = () => {
   return <>
-    {/* <Canvas
+    <Canvas
       style={{ height: '100vh', width: '100vw' }}
       camera={{ position: [0, 0, 2], fov: 75, near: 1, far: 10}}
     >
       <ambientLight />
       <MainScene />
       <OrbitControls />
-    </Canvas> */}
+    </Canvas>
   </>;
 };
