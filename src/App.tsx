b10216c513072b7f78cad521d8a1fb27b704e628
ddforum <ddtch@yahoo.com>
2024-07-30 22:45:03 -0400

    manifest test

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,15 @@ import { useState } from "react";
 import WebApp from "@twa-dev/sdk";
 import { AppChecker } from "./AppChecker";
 import { BrowserRouter } from "react-router-dom";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <TonConnectUIProvider manifestUrl="https://ddtch.github.io/nolotto/tonconnect-manifest.json">
       <BrowserRouter>
         <AppChecker />
       </BrowserRouter>
-    </>
+    </TonConnectUIProvider>
   );
 }
 
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,17 +1,11 @@
+import { TonConnectButton } from "@tonconnect/ui-react";
 import React from "react";
-import { Canvas, useFrame } from '@react-three/fiber'
-import { MainScene } from "../scenes/MainScene";
-import { OrbitControls } from "@react-three/drei";
 
 export const LoginPage = () => {
-  return <>
-    <Canvas
-      style={{ height: '100vh', width: '100vw' }}
-      camera={{ position: [0, 0, 2], fov: 75, near: 1, far: 10}}
-    >
-      <ambientLight />
-      <MainScene />
-      <OrbitControls />
-    </Canvas>
-  </>;
+  return (
+    <>
+      1
+      <TonConnectButton />
+    </>
+  );
 };
