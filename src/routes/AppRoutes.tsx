749169eb4161590bcb7a0a03691332da9496e986
ddforum <ddtch@yahoo.com>
2024-09-24 23:29:55 -0400

    check standard import

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,37 +2,37 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import { MainApp } from "../modules/MainApp";
-// import { HomePage } from "../pages/HomePage";
-// import { GamePage } from "../pages/GamePage";
-// import { TasksPage } from "../pages/TasksPage";
-// import { LottoPage } from "../pages/LottoPage";
-// import { ProfilePage } from "../pages/ProfilePage";
+import { HomePage } from "../pages/HomePage";
+import { GamePage } from "../pages/GamePage";
+import { TasksPage } from "../pages/TasksPage";
+import { LottoPage } from "../pages/LottoPage";
+import { ProfilePage } from "../pages/ProfilePage";
 
 // const MainApp = React.lazy(() =>
 //   import("../modules/MainApp").then((module) => ({ default: module.MainApp }))
 // );
 
-const HomePage = React.lazy(() =>
-  import("../pages/HomePage").then((module) => ({ default: module.HomePage }))
-);
+// const HomePage = React.lazy(() =>
+//   import("../pages/HomePage").then((module) => ({ default: module.HomePage }))
+// );
 
-const GamePage = React.lazy(() =>
-  import("../pages/GamePage").then((module) => ({ default: module.GamePage }))
-);
+// const GamePage = React.lazy(() =>
+//   import("../pages/GamePage").then((module) => ({ default: module.GamePage }))
+// );
 
-const TasksPage = React.lazy(() =>
-  import("../pages/TasksPage").then((module) => ({ default: module.TasksPage }))
-);
+// const TasksPage = React.lazy(() =>
+//   import("../pages/TasksPage").then((module) => ({ default: module.TasksPage }))
+// );
 
-const ProfilePage = React.lazy(() =>
-  import("../pages/ProfilePage").then((module) => ({
-    default: module.ProfilePage,
-  }))
-);
+// const ProfilePage = React.lazy(() =>
+//   import("../pages/ProfilePage").then((module) => ({
+//     default: module.ProfilePage,
+//   }))
+// );
 
-const LottoPage = React.lazy(() =>
-  import("../pages/LottoPage").then((module) => ({ default: module.LottoPage }))
-);
+// const LottoPage = React.lazy(() =>
+//   import("../pages/LottoPage").then((module) => ({ default: module.LottoPage }))
+// );
 
 const AppRoutes = () => {
   return (
